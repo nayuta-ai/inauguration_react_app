826fb58b3a5798999a87547ae4db78350e4be15a
nayuta-ai <nayuta723@gmail.com>
2021-08-28 04:32:25 +0000

    front-end

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
-const [message,setMessage] = useState("hoge")
-
-useEffect(() => {
-fetch('http://localhost:5000/api/users')
-.then(response => response.json())
-.then(data => setMessage(data.message))
-},[])
-
-return (
-<div>
-hello:{message}
-</div>
-)
+    const [users, setUsers] = useState({name:'一郎'})
+    useEffect(() => {
+      fetch('http://localhost:5000/api/users')
+        .then((res) => res.json())
+        .then((data) => setUsers(data))
+    })
+    return (
+      <div>
+          {users.name}
+      </div>
+    )
 }
-
 export default App
